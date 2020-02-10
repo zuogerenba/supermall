748606b7378e66943e1cc6adcc1b2ff20b5c71e5
zuogerenba <1732935973@qq.com>
2020-02-10 20:11:30 +0800

    详情页

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,39 +1,65 @@
 <template>
   <div id="detail">
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :topImages="topImages"/>
-  
+    <detail-nav-bar />
+    <scroll class="content">
+      <div>
+        <detail-swiper :topImages="topImages" />
+        <detail-base-info :goods="goods" />
+        <detail-shop-info :shop="shop" />
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script>
 import DetailNavBar from "./detailChild/DetailNavBar";
-import DetailSwiper from "./detailChild/DetailSwiper"
-import { getDetail } from "network/detail";
+import DetailSwiper from "./detailChild/DetailSwiper";
+import DetailBaseInfo from "./detailChild/DetailBaseInfo";
+import DetailShopInfo from "./detailChild/DetailShopInfo";
+
+import Scroll from "components/common/scroll/Scroll";
+
+import { getDetail, Goods, Shop } from "network/detail";
 
 export default {
   name: "Detail",
   data() {
     return {
       iid: null,
-      topImages: []
+      topImages: [],
+      goods: {},
+      shop: {}
     };
   },
 
   components: {
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    Scroll
   },
   created() {
-    // console.log(this.$route.params);
-
+    //1.保存传入的id
     this.iid = this.$route.params.iid;
 
     //2.根据iid请求数据
     getDetail(this.iid).then(res => {
       console.log(res);
-      this.topImages = res.result.itemInfo.topImages;
+      //1.获取轮播图图片
+      const data = res.result;
+      this.topImages = data.itemInfo.topImages;
       // console.log(this.topImages);
+      //2.获取整合后的标题，配送，价格信息
+      this.goods = new Goods(
+        data.itemInfo,
+        data.columns,
+        data.shopInfo.services
+      );
+      // console.log(this.goods);
+      //3.获取商家店铺信息
+      this.shop = new Shop(data.shopInfo);
+      console.log(this.shop);
     });
   },
   mounted() {},
@@ -42,4 +68,20 @@ export default {
 </script>
 
 <style scoped>
+#detail {
+  position: relative;
+  z-index: 1;
+  background-color: #fff;
+  height: 100vh;
+}
+.content {
+  height: calc(100% - 54px);
+  /* height: 530px; */
+  /* overflow: hidden;
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0; */
+}
 </style>
